829911083aca2f04bfb60c6a1443d6ba3f12c643
Joaquin Fernandez <copako12@gmail.com>
2020-03-17 23:49:16 -0300

    asd

--- src/menu.js
+++ src/menu.js
@@ -5,15 +5,15 @@ let menuModule = (function() {
 	menu.innerHTML = `
     <div class="menu__item1 menu__item">
     <h3>Pizza Cielo<span> $550</span></h3>
-    <img src="/src/img/pizza_cielo.png" alt="" />
+    <img src="./img/pizza_cielo.png" alt="" />
 </div>
 <div class="menu__item2 menu__item">
     <h3>Pizza Rey<span> $350</span></h3>
-    <img src="/src/img/pizza_rey.jpg" alt="" />
+    <img src="./img/pizza_rey.jpg" alt="" />
 </div>
 <div class="menu__item3  menu__item">
     <h3>Pizza Rambla<span> $450</span></h3>
-    <img src="/src/img/pizza_rambla.png" alt="" />
+    <img src="./img/pizza_rambla.png" alt="" />
 </div>`;
 
 	return {
